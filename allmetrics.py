8ebabea74c2726a6525c8ab0ddb3ccbd62655f70
dileep <dileepdk2399@gmail.com>
2025-11-10 10:32:09 +0000

    added nprobes for ivf query

--- allmetrics.py
+++ allmetrics.py
@@ -12,6 +12,16 @@ def dist_query(query_vecs:np.ndarray,top_k_vecs:int,dist_func,cur:psycopg2.exten
             SELECT id FROM {TABLE_NAME}
             ORDER BY embedding {utils.dist_functions[dist_func]} %s
             LIMIT {top_k_vecs};""",cur,logger,args=(query_vector_string,))
+        
+
+def dist_query_ivf(query_vecs:np.ndarray,top_k_vecs:int,dist_func,cur:psycopg2.extensions.cursor,TABLE_NAME,logger:utils.clogger,n_probes):
+    for vec in query_vecs:
+        query_vector_string = str(vec.tolist()) 
+        execute_for_time(f"""
+            SET ivfflat.probes = {n_probes};
+            SELECT id FROM {TABLE_NAME}
+            ORDER BY embedding {utils.dist_functions[dist_func]} %s
+            LIMIT {top_k_vecs};""",cur,logger,args=(query_vector_string,))
 
 def get_results(query_vecs:np.ndarray,top_k_vecs:int,dist_func,cur:psycopg2.extensions.cursor,TABLE_NAME,logger:utils.clogger):
     results = []
